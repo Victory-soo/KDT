364544c689325f2ca5d3e23ade6744ed502801a1
Victory-soo <victory-soo@naver.com>
2022-07-19 11:19:25 +0900

    12days

---
+++ Class/12days/ex_11.js
@@ -0,0 +1,32 @@
+// 클릭 된 요소를 저장하기 위한 전역 변수
+let targetEl;
+
+calendar.addEventListener("click", function (e) {
+  // 클릭된 요소의 종류에 따라 각각의 기능을 수행
+  if (e.target.tagName === "P") {
+    // 날짜(P 태그)가 클릭이 되면 부모 요소를 전역 변수에 저장하여 위치 전달
+    date.value = e.target.textContent;
+    targetEl = e.target.parentNode;
+  } else if (e.target.tagName === "DIV") {
+    // 추가 된 TASK(DIV 요소로 추가)가 클릭이 되면 삭제
+    e.target.remove();
+  } else {
+    // 테이블 TD가 클릭 되면 TD를 전역 변수에 저장하여 위치 전달
+    date.value = e.target.firstChild.textContent;
+    targetEl = e.target;
+  }
+});
+
+// 작성 버튼이 클릭 되면 실행되는 함수
+function writeSchedule() {
+  let content = document.querySelector("#content");
+
+  let addEl = document.createElement("div");
+  addEl.innerText = content.value;
+
+  // 전역 변수에 저장된 요소의 위치를 사용하여 입력 받은 input 내용을 추가
+  targetEl.append(addEl);
+
+  // input의 text를 초기화
+  content.value = "";
+}
