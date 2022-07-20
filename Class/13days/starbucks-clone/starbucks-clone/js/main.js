51cfffbc951e8403b82cefccda199820a4a502d6
Victory-soo <victory-soo@naver.com>
2022-07-20 15:04:50 +0900

    Create main.js

---
+++ Class/13days/starbucks-clone/starbucks-clone/js/main.js
@@ -0,0 +1,12 @@
+// SUBMENU > SEARCH
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+searchEl.addEventListener("click", function() {
+    searchInputEl.focus();
+});
+searchInputEl.addEventListener("focus", function() {
+    searchInputEl.setAttribute("placeholder", "통합검색");
+});
+searchInputEl.addEventListener("blur", function() {
+    searchInputEl.setAttribute("placeholder", "");
+});
