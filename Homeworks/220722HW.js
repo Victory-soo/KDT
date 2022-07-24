a6ea6cafe2c741cd5495e72cbdd274a70b1b0c98
Victory-soo <victory-soo@naver.com>
2022-07-24 12:51:13 +0900

    220722HW

--- Homeworks/220722HW.js
+++ Homeworks/220722HW.js
@@ -1,6 +1,6 @@
 // 핸드폰 번호 가리기
-
 let phone_number = "01026482367";
+
 function solution1(phone_number) {
     var answer = '';
     let cut = "*";
@@ -17,8 +17,8 @@ console.log("");
 
 // 자릿수 더하기
 let n=1234;
-function solution2(n)
-{
+
+function solution2(n){
     var answer = 0;
     let str = [...String(n)];
     for (i=0; i<str.length; i++) {
@@ -35,6 +35,7 @@ console.log("");
 
 // 없는 숫자 더하기
 let numbers = [1,2,3,4,6,7,8,0];
+
 function solution3(numbers) {
     var answer = 0;
     let stndrd = [0,1,2,3,4,5,6,7,8,9];
@@ -47,3 +48,24 @@ function solution3(numbers) {
 console.log("<없는 숫자 더하기>")
 console.log(solution3(numbers));
 console.log("");
+
+
+
+
+// 나누어 떨어지는 숫자 배열
+let arr = [3,2,6];
+let divisor = 10;
+
+function solution(arr, divisor) {
+    var answer = [];
+    answer = arr.filter( num => num % divisor == 0);
+    answer.sort(function(a,b) {
+        return a - b;
+    });
+    if (answer.length == 0) {
+        answer = [-1];
+    }
+    return answer;
+}
+console.log("<나누어 떨어지는 숫자 배열>")
+console.log(solution(arr,divisor));
