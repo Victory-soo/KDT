d0511619ec6c5c492ea3c1489d4c3b9cf690c800
Victory-soo <victory-soo@naver.com>
2022-08-01 18:35:02 +0900

    18days

--- Class/18days/music/js/main.js
+++ Class/18days/music/js/main.js
@@ -1,2 +1,43 @@
 const frame = document.querySelector("section");
 const list = frame.querySelectorAll("article");
+const len = list.length;
+const deg = 360 / len;
+
+for (let i = 0; i < len; i++) {
+  list[i].style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
+}
+
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
+let num = 0;
+let active = 0;
+
+prev.addEventListener("click", function (e) {
+  frame.style.transform = `rotate(${deg * ++num}deg)`;
+
+  if (active === 0) {
+    active = len - 1;
+  } else {
+    active--;
+  }
+
+  for (let el of list) {
+    el.classList.remove("on");
+  }
+  list[active].classList.add("on");
+});
+
+next.addEventListener("click", function (e) {
+  frame.style.transform = `rotate(${deg * --num}deg)`;
+
+  if (active === len - 1) {
+    active = 0;
+  } else {
+    active++;
+  }
+
+  for (el of list) {
+    el.classList.remove("on");
+  }
+  list[active].classList.add("on");
+});
